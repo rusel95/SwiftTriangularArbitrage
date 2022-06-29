da078f6dd6e99e292c54ac9dd4c264af191ab9e7
Ruslan Pppesku <ruslanpopesku95@gmail.com>
2022-06-29 16:50:58 +0300

    [refactor] remove ability to turn on same Job several times

--- Sources/App/Handlers/DefaultBotHandler.swift
+++ Sources/App/Handlers/DefaultBotHandler.swift
@@ -75,13 +75,18 @@ private extension DefaultBotHandlers {
         let handler = TGCommandHandler(commands: [Mode.logging.command]) { [weak self] update, bot in
             guard let self = self else { return }
             
-            let infoMessage = "Now you will see market updates every \(Int(Mode.logging.jobInterval / 60)) minutes\n\(self.resultsFormatDescription)"
-            _ = try? bot.sendMessage(params: .init(chatId: .chat(update.message!.chat.id), text: infoMessage))
-            self.loggingJob = Jobs.add(interval: .seconds(Mode.logging.jobInterval)) { [weak self] in
-                self?.printMarketPosibilities(for: BinanceAPIService.Crypto.allCases,
-                                              paymentMethods: [.privatbank, .monobank],
-                                              bot: bot,
-                                              update: update)
+            if self.loggingJob?.isRunning != nil {
+                let infoMessage = "Logging Updates already running!"
+                _ = try? bot.sendMessage(params: .init(chatId: .chat(update.message!.chat.id), text: infoMessage))
+            } else {
+                let infoMessage = "Now you will see market updates every \(Int(Mode.logging.jobInterval / 60)) minutes\n\(self.resultsFormatDescription)"
+                _ = try? bot.sendMessage(params: .init(chatId: .chat(update.message!.chat.id), text: infoMessage))
+                self.loggingJob = Jobs.add(interval: .seconds(Mode.logging.jobInterval)) { [weak self] in
+                    self?.printMarketPosibilities(for: BinanceAPIService.Crypto.allCases,
+                                                  paymentMethods: [.privatbank, .monobank],
+                                                  bot: bot,
+                                                  update: update)
+                }
             }
         }
         bot.connection.dispatcher.add(handler)
@@ -92,14 +97,19 @@ private extension DefaultBotHandlers {
         let handler = TGCommandHandler(commands: [Mode.trading.command]) { [weak self] update, bot in
             guard let self = self else { return }
             
-            let infoMessage = "Now you will see market updates every \(Int(Mode.trading.jobInterval)) seconds \n\(self.resultsFormatDescription)"
-            _ = try? bot.sendMessage(params: .init(chatId: .chat(update.message!.chat.id), text: infoMessage))
-            
-            self.alertingJob = Jobs.add(interval: .seconds(Mode.trading.jobInterval)) { [weak self] in
-                self?.printMarketPosibilities(for: BinanceAPIService.Crypto.allCases,
-                                              paymentMethods: [.privatbank, .monobank],
-                                              bot: bot,
-                                              update: update)
+            if self.tradingJob?.isRunning != nil {
+                let infoMessage = "Trading Updates already running!"
+                _ = try? bot.sendMessage(params: .init(chatId: .chat(update.message!.chat.id), text: infoMessage))
+            } else {
+                let infoMessage = "Now you will see market updates every \(Int(Mode.trading.jobInterval)) seconds \n\(self.resultsFormatDescription)"
+                _ = try? bot.sendMessage(params: .init(chatId: .chat(update.message!.chat.id), text: infoMessage))
+                
+                self.tradingJob = Jobs.add(interval: .seconds(Mode.trading.jobInterval)) { [weak self] in
+                    self?.printMarketPosibilities(for: BinanceAPIService.Crypto.allCases,
+                                                  paymentMethods: [.privatbank, .monobank],
+                                                  bot: bot,
+                                                  update: update)
+                }
             }
         }
         bot.connection.dispatcher.add(handler)
@@ -108,10 +118,23 @@ private extension DefaultBotHandlers {
     /// add handler for command "/start_alerting"
     func commandStartAlertingHandler(app: Vapor.Application, bot: TGBotPrtcl) {
         let handler = TGCommandHandler(commands: [Mode.alerting.command]) { [weak self] update, bot in
-            let text = "Start handling Extra opportinuties: "
-            _ = try? bot.sendMessage(params: .init(chatId: .chat(update.message!.chat.id), text: text))
-            self?.alertingJob = Jobs.add(interval: .seconds(Mode.alerting.jobInterval)) { [weak self] in
-                self?.checkWhiteBitArbitrage(for: bot, update: update)
+            guard let self = self else { return }
+            
+            let profitableSpread = 0.3
+            let extraOpportunities = """
+            Binance - Mono - WhiteBit - Binance  (with spread >= \(profitableSpread.toLocalCurrency()))
+            Binance - WhiteBit - MonoBank(Any Bank) - Binance  (with spread >= \(profitableSpread.toLocalCurrency()))
+            """
+            
+            if self.alertingJob?.isRunning != nil {
+                _ = try? bot.sendMessage(params: .init(chatId: .chat(update.message!.chat.id),
+                                                       text: "Already handling Extra opportinuties:\n\(extraOpportunities)"))
+            } else {
+                _ = try? bot.sendMessage(params: .init(chatId: .chat(update.message!.chat.id),
+                                                       text: "Started handling Extra opportinuties:\n\(extraOpportunities)"))
+                self.alertingJob = Jobs.add(interval: .seconds(Mode.alerting.jobInterval)) { [weak self] in
+                    self?.checkWhiteBitArbitrage(profitableSpread: profitableSpread, bot: bot, update: update)
+                }
             }
         }
         bot.connection.dispatcher.add(handler)
@@ -210,11 +233,7 @@ private extension DefaultBotHandlers {
         }
     }
     
-    func checkWhiteBitArbitrage(for bot: TGBotPrtcl, update: TGUpdate) {
-        let worthOfProfitSpread = 0.3
-        let opportinityDescription = "Binance - Mono - WhiteBit - Binance  (>= \(worthOfProfitSpread.toLocalCurrency()))\nBinance - WhiteBit - MonoBank(Any Bank) - Binance  (>= \(worthOfProfitSpread.toLocalCurrency()))"
-        _ = try? bot.sendMessage(params: .init(chatId: .chat(update.message!.chat.id), text: opportinityDescription))
-        
+    func checkWhiteBitArbitrage(profitableSpread: Double, bot: TGBotPrtcl, update: TGUpdate) {
         var whiteBitAsks: [Double]?
         var whiteBitBids: [Double]?
         var monoPricesInfo: PricesInfo? = nil
@@ -239,11 +258,11 @@ private extension DefaultBotHandlers {
                 return
             }
 
-            if monoPricesInfo.possibleSellPrice - whiteBitBuy > worthOfProfitSpread {
+            if monoPricesInfo.possibleSellPrice - whiteBitBuy > profitableSpread {
                 // If prices for Buying on WhiteBit is Much more lower then prices for selling on Monobank
                 let text = "OPPORTINITY!    Mono Sell: \(monoPricesInfo.possibleBuyPrice.toLocalCurrency()) - WhiteBit buy: \(whiteBitBuy.toLocalCurrency()) = \((monoPricesInfo.possibleBuyPrice - whiteBitBuy).toLocalCurrency())"
                 _ = try? bot.sendMessage(params: .init(chatId: .chat(update.message!.chat.id), text: text))
-            } else if whiteBitSell - monoPricesInfo.possibleBuyPrice > worthOfProfitSpread {
+            } else if whiteBitSell - monoPricesInfo.possibleBuyPrice > profitableSpread {
                 // If prices for Selling on White bit much more lower then prices for buying on Monobank
                 let text = "OPPORTINITY!    WhiteBit sell: \(whiteBitSell.toLocalCurrency()) - Mono Buy: \(monoPricesInfo.possibleBuyPrice.toLocalCurrency()) = \((whiteBitSell - monoPricesInfo.possibleBuyPrice).toLocalCurrency())"
                 _ = try? bot.sendMessage(params: .init(chatId: .chat(update.message!.chat.id), text: text))
