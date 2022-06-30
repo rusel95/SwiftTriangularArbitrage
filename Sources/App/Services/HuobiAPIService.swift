094d085ae900da127b4486eab620c849dd061801
Ruslan Pppesku <ruslanpopesku95@gmail.com>
2022-06-30 12:27:18 +0300

    [fix] Huobi service compling

--- Sources/App/Services/HuobiAPIService.swift
+++ Sources/App/Services/HuobiAPIService.swift
@@ -20,7 +20,7 @@ final class HuobiAPIService {
 
     // MARK: - Structs
     
-    struct Response: Codable {
+    struct MarketData: Codable {
         let ch, status: String
         let ts: Int
         let tick: Tick
@@ -39,26 +39,26 @@ final class HuobiAPIService {
     // MARK: - METHODS
     
     func getOrderbook(
-        for asset: Asset,
-        completion: @escaping(_ asks: [Double]?, _ bids: [Double]?, _ error: Error?) -> Void
+        symbol: Symbol = .usdtuah,
+        completion: @escaping(_ asks: [Double], _ bids: [Double], _ error: Error?) -> Void
     ) {
-        var urlComponents = URLComponents(string: "https://whitebit.com/api/v4/public/orderbook/\(asset.rawValue)")!
+        var urlComponents = URLComponents(string: "https://api.huobi.pro/market/depth")!
         urlComponents.queryItems = [
-            URLQueryItem(name: "limit", value: "5")
+            URLQueryItem(name: "symbol", value: symbol.rawValue),
+            URLQueryItem(name: "type", value: "step0")
         ]
         let request = URLRequest(url: urlComponents.url!)
         URLSession.shared.dataTask(with: request, completionHandler: { data, response, error in
             if let error = error {
                 print("error is \(error.localizedDescription)")
-                completion(nil, nil, error)
+                completion([], [], error)
                 return
             }
             
-            guard let data = data else { return }
+            guard let data = data, let marketData = try? JSONDecoder().decode(MarketData.self, from: data) else { return }
             
-            let orderbookData = try? JSONDecoder().decode(OrderbookData.self, from: data)
-            let asks = orderbookData?.asks.compactMap { $0.first }.compactMap { Double($0) }
-            let bids = orderbookData?.bids.compactMap { $0.first }.compactMap { Double($0) }
+            let asks = marketData.tick.asks.compactMap { $0.first }.compactMap { Double($0) }
+            let bids = marketData.tick.bids.compactMap { $0.first }.compactMap { Double($0) }
             completion(asks, bids, nil)
         }).resume()
     }
