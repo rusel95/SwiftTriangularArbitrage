69b835d8eb5f49133f38bf2f59c6a4245cf53faf
Ruslan Pppesku <ruslanpopesku95@gmail.com>
2022-07-14 14:27:49 +0300

    [fix] UserDefaults compiling

--- Sources/App/Utils/UserInfoProvider.swift
+++ Sources/App/Utils/UserInfoProvider.swift
@@ -11,7 +11,7 @@ import FoundationNetworking
 #endif
 import telegram_vapor_bot
 
-final class UserInfoProvider {
+final class UserInfoProvider: NSObject {
     
     // MARK: - TYPEALIAS
 
@@ -24,19 +24,19 @@ final class UserInfoProvider {
     // MARK: - METHODS
     
     func getUser(chatId: Int64) -> TGUser? {
-        guard let data = UserDefaults.standard.value(forKey: String(chatId)) as? Data else { return nil }
+        guard let data = Foundation.UserDefaults.standard.value(forKey: String(chatId)) as? Data else { return nil }
         
         return try? JSONDecoder().decode(TGUser.self, from: data)
     }
     
     func set(user: TGUser, chatId: Int64) {
         if let encoded = try? JSONEncoder().encode(user) {
-            UserDefaults.standard.set(encoded, forKey: String(chatId))
+            Foundation.UserDefaults.standard.set(encoded, forKey: String(chatId))
         }
     }
     
     func getAllUsersInfo() -> [UserInfo] {
-        return UserDefaults.standard
+        return Foundation.UserDefaults.standard
             .dictionaryRepresentation()
             .compactMap { (key, value) in
                 guard let chatId = Int64(key), let user = getUser(chatId: chatId) else { return nil }
