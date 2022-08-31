1fa6965af3da7b0a10b3e65491a28c601a972324
Ruslan Popesku <ruslanpopesku95@gmail.com>
2022-08-31 14:46:11 +0300

    [add] basic arbitrage pairs collecting

--- Sources/App/Handlers/ArbitrageCalculator.swift
+++ Sources/App/Handlers/ArbitrageCalculator.swift
@@ -22,45 +22,84 @@ final class ArbitrageCalculator {
         BinanceAPIService.shared.getAllBookTickers { tickers in
             guard let tickers = tickers else { return }
 
-            // TODO: - use base/quote instead of this hard String
-            let tickersToCount = tickers.filter { $0.symbol.count == 6 }
+            let pairsToCount = tickers
+                .filter { $0.symbol.count == 6 } // TODO: - use base/quote instead of this hard String
+                .map { $0.symbol }[0...100] // TODO: - optimize to get full amout
             
-            var triangularPairsList: [BinanceAPIService.BookTicker] = []
-            var temoveDuplicatesLst: [BinanceAPIService.BookTicker] = []
+            let start = CFAbsoluteTimeGetCurrent()
             
-            // Get Pair A
+            var triangularPairsSet: Set<[String: String]> = Set()
+            var removeDuplicatesSet: Set<[String]> = Set()
+            
+            // Get Pair A - Start from A
             // NOTE - should make https://api.binance.com/api/v3/exchangeInfo request to now that
-            for tickerA in tickersToCount {
-                let pairBoxA = tickerA.symbol.splitStringInHalf()
-                let aBase = pairBoxA.firstHalf
-                let aQuote = pairBoxA.secondHalf
+            for pairA in pairsToCount {
+                let pairASplit = pairA.splitStringInHalf()
+                let aBase = pairASplit.firstHalf
+                let aQuote = pairASplit.secondHalf
                 
-                // Get Pair B
-                for tickerB in tickersToCount {
-                    let pairBoxB = tickerB.symbol.splitStringInHalf()
-                    let bBase = pairBoxB.firstHalf
-                    let bQuote = pairBoxB.secondHalf
+                // Get Pair B - Find B pair where one coint matched
+                for pairB in pairsToCount {
+                    let pairBSplit = pairB.splitStringInHalf()
+                    let bBase = pairBSplit.firstHalf
+                    let bQuote = pairBSplit.secondHalf
                     
-                    if pairBoxB != pairBoxA {
-                        if (pairBoxA.firstHalf == bBase || pairBoxA.secondHalf == bBase) ||
-                            (pairBoxA.firstHalf == bQuote || pairBoxA.secondHalf == bQuote) {
+                    if pairBSplit != pairASplit {
+                        if (pairASplit.firstHalf == bBase || pairASplit.secondHalf == bBase) ||
+                            (pairASplit.firstHalf == bQuote || pairASplit.secondHalf == bQuote) {
                             
-                            // Get Pair C
+                            // Get Pair C - Find C pair where base and quote exist in A and B configurations
+                            for pairC in pairsToCount {
+                                let pairCSplit = pairC.splitStringInHalf()
+                                let cBase = pairCSplit.firstHalf
+                                let cQuote = pairCSplit.secondHalf
+                                
+                                // Count the number of matching C items
+                                if pairCSplit != pairASplit && pairCSplit != pairBSplit {
+                                    let combineAll = [pairA, pairB, pairC]
+                                    let pairBox = [aBase, aQuote, bBase, bQuote, cBase, cQuote]
+                                    
+                                    let cBaseCount = pairBox.filter { $0 == cBase }.count
+                                    let cQuoteCount = pairBox.filter { $0 == cQuote }.count
+                                    
+                                    // Determining Triangular Match
+                                    if cBaseCount == 2 && cQuoteCount == 2 && cBase != cQuote {
+                                        let uniqueItem = combineAll.sorted()
+                                        removeDuplicatesSet.insert(uniqueItem)
+                                        let matchDict: [String: String] = [
+                                            "a_base": aBase,
+                                            "b_base": bBase,
+                                            "c_base": cBase,
+                                            "a_quote": aQuote,
+                                            "b_quote": bQuote,
+                                            "c_quote": cQuote,
+                                            "pair_a": pairA,
+                                            "pair_b": pairB,
+                                            "pair_c": pairC,
+                                            "combined": uniqueItem.joined(separator: "_")
+                                        ]
+                                        triangularPairsSet.insert(matchDict)
+                                    }
+                                }
+                            }
                         }
                     }
                 }
             }
+            
+            let diff = CFAbsoluteTimeGetCurrent() - start
+            print(diff, " seconds\n", triangularPairsSet.count)
         }
     }
     
 }
 
 extension String {
-   func splitStringInHalf()->(firstHalf:String,secondHalf:String) {
-        let words = self.components(separatedBy: " ")
-        let halfLength = words.count / 2
-        let firstHalf = words[0..<halfLength].joined(separator: " ")
-        let secondHalf = words[halfLength..<words.count].joined(separator: " ")
-        return (firstHalf:firstHalf,secondHalf:secondHalf)
+    
+   func splitStringInHalf() -> (firstHalf: String, secondHalf: String) {
+        let firstHalf = self.prefix(3)
+        let secondHalf = self.suffix(3)
+        return (firstHalf: String(firstHalf), secondHalf: String(secondHalf))
     }
+    
 }
