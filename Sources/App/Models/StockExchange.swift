c201c88201163d4785bc9878f36d37f72b7c5170
Ruslan Popesku <ruslanpopesku95@gmail.com>
2023-01-20 18:05:19 +0200

    [update] turn off exmo

--- Sources/App/Models/StockExchange.swift
+++ Sources/App/Models/StockExchange.swift
@@ -43,7 +43,7 @@ enum StockExchange: String, CaseIterable {
     
     var isTurnedOn: Bool {
         switch self {
-        case .whitebit, .kucoin:
+        case .whitebit, .exmo:
             return false
         default:
             return true
